3d10f31c70ab12a6ec42dfe55a3bed16a6a14e9c
Rachel Brendel <brendelrachel@gmail.com>
2023-06-08 13:04:59 -0500

    Add back to home button and functionality and styling for completed task

--- src/components/Completed/completedActivites.js
+++ src/components/Completed/completedActivites.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './completedActivites.css';
 
 const CompletedActivities = ({ completedActivities }) => {
+  const history = useHistory();
+
+  const handleReturnHome = () => {
+    history.push('/activity');
+  };
+
   if (!completedActivities || !Array.isArray(completedActivities)) {
-    return null; // Return null or a placeholder component if the prop is invalid
+    return null;
   }
 
   return (
-    <div>
+    <div className="completed-activities-container">
       <h3>Completed Activities</h3>
       <ul>
         {completedActivities.map((activity, index) => (
-          <li key={index}>{activity}</li>
+          <li key={index}>
+            <span className="list-icon">&#10003;</span>&nbsp;{activity}
+          </li>
         ))}
       </ul>
+      <button className="return-button" onClick={handleReturnHome}>
+        Return to Home
+      </button>
     </div>
   );
 };
+
 
 export default CompletedActivities;
