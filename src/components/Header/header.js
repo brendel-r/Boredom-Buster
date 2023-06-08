f887d3b7d843ba95ae9287888de5875be4b617ae
Rachel Brendel <brendelrachel@gmail.com>
2023-06-07 21:57:30 -0500

    Revert "fix/remove unused code"
    This reverts commit 247038b72c2389e4255a6187555f4ea29f7b0f15.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,3 +1,62 @@
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import './header.css';
+// import logo from '../../assets/bb-logo.png';
+// import slogans from '../../assets/Slogans.js';
+
+// const Header = () => {
+//   const [randomSlogan, setRandomSlogan] = useState('');
+
+//   useEffect(() => {
+//     const randomIndex = Math.floor(Math.random() * slogans.length);
+//     setRandomSlogan(slogans[randomIndex]);
+//   }, []);
+
+//   return (
+//     <header>
+//       <div className="header-container">
+//       <Link to="/">
+//         <img src={logo} id="header-logo" alt="Boredom Buster Logo" />
+//       </Link>
+//       <h1 className="slogan">{randomSlogan}</h1>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import './header.css';
+// import logo from '../../assets/bb-logo.png';
+// import slogans from '../../assets/Slogans.js';
+
+// const Header = ({ isDarkMode, toggleMode }) => {
+//   const [randomSlogan, setRandomSlogan] = useState('');
+
+//   useEffect(() => {
+//     const randomIndex = Math.floor(Math.random() * slogans.length);
+//     setRandomSlogan(slogans[randomIndex]);
+//   }, []);
+
+//   return (
+//     <header>
+//       <div className="header-container">
+//         <Link to="/">
+//           <img src={logo} id="header-logo" alt="Boredom Buster Logo" />
+//         </Link>
+//         <button onClick={toggleMode} className="mode-toggle-button">
+//           {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+//         </button>
+//         <h1 className="slogan">{randomSlogan}</h1>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import './header.css';
