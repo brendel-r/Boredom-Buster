c4adc7a4a644beebf357458309ec501bb7fde973
Rachel Brendel <brendelrachel@gmail.com>
2023-06-08 14:22:07 -0500

    Fix the header logo to tatake user back to home page

--- src/App.js
+++ src/App.js
@@ -7,53 +7,6 @@ import Header from './components/Header/header';
 import Activity from './components/Activity/activity';
 import CompletedActivities from './components/Completed/completedActivites';
 
-// function App() {
-//   const [data, setData] = useState({});
-//   const [isDarkMode, setIsDarkMode] = useState(true);
-//   const [completedActivities, setCompletedActivities] = useState([]);
-
-//   const toggleMode = () => {
-//     setIsDarkMode(!isDarkMode);
-//   };
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetchAPI('http://www.boredapi.com/api/activity/');
-//         setData(response);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <Router>
-//       <div className={`app-container ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-//         <Header isDarkMode={isDarkMode} toggleMode={toggleMode} />
-
-//         <Switch>
-//           <Route exact path="/completed">
-//             <CompletedActivities completedActivities={completedActivities} />
-//           </Route>
-//           <Route path="/">
-//             <Activity
-//               data={data}
-//               completedActivities={completedActivities}
-//               setCompletedActivities={setCompletedActivities}
-//             />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 function App() {
   const [data, setData] = useState({});
   const [isDarkMode, setIsDarkMode] = useState(true);
