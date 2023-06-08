3a7546ba987c4ba212824cd849db248385e96c8d
Rachel Brendel <brendelrachel@gmail.com>
2023-06-07 21:26:02 -0500

    Add functionality to the skip button on activity card

--- src/components/Activity/activity.js
+++ src/components/Activity/activity.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchAllData } from '../../apiCalls';
 import './activity.css';
 
-const Activity = ({ data }) => {
+const Activity = () => {
+  const [currentActivity, setCurrentActivity] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const [data] = await fetchAllData();
+      setCurrentActivity(data);
+    } catch (error) {
+      console.error('Error fetching activity:', error);
+    }
+  };
+
+  const handleSkipClick = async () => {
+    try {
+      const [data] = await fetchAllData();
+      setCurrentActivity(data);
+    } catch (error) {
+      console.error('Error fetching new activity:', error);
+    }
+  };
+
   return (
     <div className='activity-card'>
-      <h2 className='current-activity'>{data.activity}</h2>
-      <p className='current-activity-type'>Type: {data.type}</p>
-      <p className='current-activity-people'>Participants: At least {data.participants} </p>
+      {currentActivity ? (
+        <>
+          <h2 className='current-activity'>{currentActivity.activity}</h2>
+          <p className='current-activity-type'>Type: {currentActivity.type}</p>
+          <p className='current-activity-people'>Participants: At least {currentActivity.participants}</p>
+        </>
+      ) : (
+        <p>Loading activity...</p>
+      )}
+
+      <div className='button-container'>
+        <button className='skip-button' onClick={handleSkipClick}>Skip</button>
+        <button className='completed-button'>Completed</button>
+      </div>
     </div>
   );
 };
