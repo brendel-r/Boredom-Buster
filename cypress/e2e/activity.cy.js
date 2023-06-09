94f346fa957c24af5b2fe80fd952898f4ae91a79
Rachel Brendel <brendelrachel@gmail.com>
2023-06-09 12:25:50 -0500

    Fix api hits on testing

--- cypress/e2e/activity.cy.js
+++ cypress/e2e/activity.cy.js
@@ -14,8 +14,8 @@ describe('see activity on page load', () => {
   
   it('skip button', () => {
     
+    cy.visit('http://localhost:3000/')
     cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture2.json' });
-    // cy.visit('http://localhost:3000/')
     cy.get('.skip-button').click();
     cy.wait(3000);
   });
