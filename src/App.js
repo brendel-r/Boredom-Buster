e449d1d436a272dc89d86d1d805a93bf07f4017b
Rachel Brendel <brendelrachel@gmail.com>
2023-06-08 23:03:02 -0500

    Add error page component for bad url handeling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/header";
 import Activity from "./components/Activity/activity";
 import CompletedActivities from "./components/Completed/completedActivites";
+import ErrorPage from "./components/Error/errorPage";
 
 function App() {
   const [data, setData] = useState({});
@@ -18,9 +19,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetchAPI(
-          "http://www.boredapi.com/api/activity/"
-        );
+        const response = await fetchAPI("http://www.boredapi.com/api/activity/");
         setData(response);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -36,17 +35,18 @@ function App() {
 
   return (
     <Router>
-      <div
-        className={`app-container ${isDarkMode ? "dark-mode" : "light-mode"}`}
-      >
+      <div className={`app-container ${isDarkMode ? "dark-mode" : "light-mode"}`}>
         <Header isDarkMode={isDarkMode} toggleMode={toggleMode} />
 
         <Switch>
           <Route exact path="/completed">
             <CompletedActivities completedActivities={completedActivities} />
           </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Activity addCompletedActivity={addCompletedActivity} />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
           </Route>
         </Switch>
       </div>
---
+++ src/components/Error/errorPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./errorPage.css";
+
+const ErrorPage = () => {
+  return <div className="error-message">This is not our page. Please check the URL.</div>;
+};
+
+export default ErrorPage;
