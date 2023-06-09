2f873479a32ff3f24a54bd29c5e058aa7f0ec749
Rachel Brendel <brendelrachel@gmail.com>
2023-06-09 12:07:52 -0500

    Add testing to the activity and skip button

--- cypress/e2e/activity.cy.js
+++ cypress/e2e/activity.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('see activity on page load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture1.json' });
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('displays the activity on page load', () => {
+    cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture1.json' });
+    cy.visit('http://localhost:3000/');
+    cy.get('.current-activity').should('be.visible');
+    cy.get('.current-activity-type').should('be.visible');
+    cy.get('.current-activity-people').should('be.visible');
+  });
+  
+  it('skip button', () => {
+    
+    cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture2.json' });
+    // cy.visit('http://localhost:3000/')
+    cy.get('.skip-button').click();
+    cy.wait(3000);
+  });
+});
