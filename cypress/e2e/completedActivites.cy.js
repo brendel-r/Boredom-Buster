3c193b16d92665cacecdbdb92443a6d490e0dcb1
Rachel Brendel <brendelrachel@gmail.com>
2023-06-09 21:18:39 -0500

    Finish cypress testing for error

--- cypress/e2e/completedActivites.cy.js
+++ cypress/e2e/completedActivites.cy.js
@@ -1,18 +1,28 @@
-
-describe('see activity on page load', () => {
+describe('completed activity', () => {
   beforeEach(() => {
     cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture1.json' });
     cy.visit('http://localhost:3000/completed');
   });
-
-  it('displays a container for completed activities', () => {
-    cy.get('.completed-activities-container').should('be.visible');
-    cy.wait(1500);
+  
+  it('should have a completed activiy list', () => {
+    cy.fixture('completedFixture.json').then((data) => {
+      expect(data).to.be.an('array');
+      expect(data).to.have.lengthOf(3);
+      expect(data[0]).to.equal('Example Activity1');
+      expect(data[1]).to.equal('Example Activity2'); 
+    });
   });
 
-  it('clicks the return button and returns to home', () => {
+  it('logo', () => {
+    cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture1.json' });
+    cy.get('img').click();
+    cy.url().should('eq', 'http://localhost:3000/');
+  });
+
+  it('return button', () => {
+    cy.intercept('GET', 'http://www.boredapi.com/api/activity', { fixture: 'activityFixture1.json' });
+
     cy.get('.return-button').click();
     cy.url().should('eq', 'http://localhost:3000/');
   });
 });
-   
---
+++ cypress/e2e/header.cy.js
@@ -0,0 +1,27 @@
+describe("header", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://www.boredapi.com/api/activity", {
+      fixture: "activityFixture1.json",
+    });
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("should have logo", () => {
+    cy.get("img").should("have.attr", "alt");
+  });
+
+  it("should have a slogan", () => {
+    cy.fixture("sloganFixture.json").then((data) => {
+      expect(data).to.be.an("array");
+      expect(data[0]).to.equal("Boredom, meet your match!");
+    });
+  });
+
+  it("should switch between light and dark mode", () => {
+    cy.get(".toggle-btn").should("have.text", "Switch to Light Mode");
+    cy.get(".toggle-btn").click();
+    cy.get(".toggle-btn").should("have.text", "Switch to Dark Mode");
+    cy.get(".toggle-btn").click();
+    cy.get(".toggle-btn").should("have.text", "Switch to Light Mode");
+  });
+});
